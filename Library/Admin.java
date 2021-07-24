243a9674690e214706306d8d625611982d58c55f
Ranjana Kambhammettu <ranjanak207@gmail.com>
2021-07-24 20:09:46 +0400

    Add Transaction and edit Admin

--- Library/Admin.java
+++ Library/Admin.java
@@ -16,6 +16,12 @@ public class Admin extends Person
     private static final int m_numBooksBorrowLimit = 5; // maximum books which a Customer can borrow
     private static final double m_fineRate = 100.0; // this value can be decided later
     private static final String m_url = "src/admin.dat";
+    private static final int m_maxBorrowDays=14;
+
+    public static int getMaxBorrowDays()
+    {
+        return m_maxBorrowDays;
+    }
 
     public Admin(String name, int age, String username, String password)
     {
