f6f2024a708709a5beec9b684409681d261ca79d
Ranjana Kambhammettu <ranjanak207@gmail.com>
2021-07-25 07:54:24 +0400

    Updated Main

--- Library/Main.java
+++ Library/Main.java
@@ -1,15 +1,53 @@
 import java.io.IOException;
-import java.util.ArrayList;
+//import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main 
 {
     public static void main(String[] args) throws IOException {
-        // Admin a = new Admin("Andrew", 35, "al.andrew@vivlio.org",
+        char choiceMain;
+        Scanner charIn = new Scanner(System.in);
+        System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("                 WELCOME TO VIVLIO LIBRARY!                   ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        do
+        {
+            System.out.println("\n\nEnter your option (1/2/3) according to the menu: ");
+            System.out.println("1. Customer");
+            System.out.println("2. Administration");
+            System.out.println("3. Exit");
+            int check=charIn.nextInt();
+            switch(check)
+            {
+                case 1:
+                //Call Customer
+                break;
+                case 2:
+                //Call Admin
+                break;
+                case 3:
+                System.out.println("Exiting the program!");
+                System.exit(0);
+                break;
+                default:
+                System.out.println("Wrong choice entered!");
+            }
+            System.out.println("Do you want to continue in Main? Enter Y if yes else press any key: ");
+            choiceMain=charIn.next().charAt(0);
+        } while (choiceMain=='Y');
+
+        charIn.close();
+        
+    }    
+}
+
+//Testing Commands: 
+// Admin a = new Admin("Andrew", 35, "al.andrew@vivlio.org",
         //         "Adm1n_paSs");
         // a.saveAdminDetails();
 
-        Admin a = Admin.getAdmin();
-        System.out.println(a);
+        //Admin a = Admin.getAdmin();
+       // System.out.println(a);
 
         // new Book();
         // //System.out.println(Book.getBooks());
@@ -23,5 +61,3 @@ public class Main
         // //System.out.println(c1.getBookWithAuthor(new String[] { "J K Rowling", "Cassandra Clare"}));
         // //System.out.println(c1.getBookWithGenre(new String[] { "Fiction" } ));
         // System.out.println(c1.getIfAvailable());
-    }    
-}
