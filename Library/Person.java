7290ebfa2aecc5b307d7e46f890af20bda62f3bd
ashy-boy786 <adarsh.kishore786@gmail.com>
2021-07-28 10:09:06 +0530

    Add diagrams

--- Library/Person.java
+++ Library/Person.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import java.io.*;
 
 public abstract class Person implements Serializable 
@@ -32,3 +33,38 @@ public abstract class Person implements Serializable
             "\nUser Name: " + m_username+ "\n");
     }
 }
+=======
+import java.io.*;
+
+public abstract class Person implements Serializable 
+{
+    protected String m_name;
+    protected int m_age;
+    protected String m_username;
+    protected String m_password;
+
+    Person() {}
+    
+    Person(String name, int age, String username, String password) {
+        m_name = name;
+        m_age = age;
+        m_username = username;
+        m_password = password;
+    }
+    public String getName() {
+        return m_name;
+    }
+    public int getAge() {
+        return m_age;
+    }
+    public String getUsername() {
+        return m_username;
+    }
+    
+    @Override
+    public String toString() {
+        return ("\nName     : " + m_name + "\nAge      : " + m_age +
+            "\nUser Name: " + m_username+ "\n");
+    }
+}
+>>>>>>> Stashed changes
