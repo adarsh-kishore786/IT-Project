32af4f2cea5b9b932a2de374574c4cf1a04276ac
ashy-boy786 <adarsh.kishore786@gmail.com>
2021-07-30 12:49:46 +0530

    Person changes

--- Library/Person.java
+++ Library/Person.java
@@ -1,39 +1,3 @@
-<<<<<<< Updated upstream
-import java.io.*;
-
-public abstract class Person implements Serializable 
-{
-    protected String m_name;
-    protected int m_age;
-    protected String m_username;
-    protected String m_password;
-    
-    Person(String name, int age, String username, String password) {
-        m_name = name;
-        m_age = age;
-        m_username = username;
-        m_password = password;
-    }
-    public String getName() {
-        return m_name;
-    }
-    public int getAge() {
-        return m_age;
-    }
-    public String getUsername() {
-        return m_username;
-    }
-    public String getPassword() {
-        return m_password;
-    }
-    
-    @Override
-    public String toString() {
-        return ("\nName     : " + m_name + "\nAge      : " + m_age + 
-            "\nUser Name: " + m_username+ "\n");
-    }
-}
-=======
 import java.io.*;
 
 public abstract class Person implements Serializable 
@@ -67,4 +31,3 @@ public abstract class Person implements Serializable
             "\nUser Name: " + m_username+ "\n");
     }
 }
->>>>>>> Stashed changes
