c1bade240d332089aef595000d5a1f1148b1f954
PoornaHegde20 <76869715+PoornaHegde20@users.noreply.github.com>
2021-08-04 19:59:21 +0530

    minor

--- Library/Main.java
+++ Library/Main.java
@@ -41,7 +41,7 @@ public class Main
     public static void main(String[] args)
     {
         // Customer cust = new Customer();
-        // // cust.saveCustomer();
+        // // Customer.saveCustomer();
         // for (Customer c : cust.getCustomers())
         //     System.out.println(c);
         //Catalogue.getBooks();
